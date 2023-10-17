24911200b6bbef106e7d1618329e2f725d2a6613
carlosismaelad <carlosismaelad@outlook.com>
2023-10-16 23:44:20 -0300

    Estruturação, primeiros componentes, rotas, formulário e css

---
+++ src/app/Moment.ts
@@ -0,0 +1,6 @@
+export interface Moment{
+    id?: number
+    title: string
+    description: string
+    image: File | string
+}
---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+  btnText = 'Compartilhar!'
+
+  async createHandler(moment: Moment){
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+    if(moment.image){
+      formData.append("image", moment.image)
+    }
+
+    //toDo
+
+    //Enviar para o service
+
+    // exibir mensagem
+
+    // redirect
+  }
+
+}
