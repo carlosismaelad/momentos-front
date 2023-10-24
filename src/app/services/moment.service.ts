f669d76ac6f82f4480da712aa1d6ba18b7264012
carlosismaelad <carlosismaelad@outlook.com>
2023-10-24 00:00:55 -0300

    Correção na formação da URL gerada para post no servidor

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -13,11 +13,13 @@ import {environment} from 'src/environments/environment'
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl
-  private apiUrl = `${this.baseApiUrl}api/moments`
+  private apiUrl = `${this.baseApiUrl}/api/moments`  
 
   constructor(private http: HttpClient) { }
 
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData)
   }
+  
 }
+
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     baseApiUrl: 'http://localhost:3333'
 };
